dd0650849bc17dcccf4ee12978f5f081894abdf0
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-14 13:04:04 -0400

    center header nav links

--- include/header.php
+++ include/header.php
@@ -15,36 +15,6 @@ require_once 'config/nav-links.php';
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbar-supported-content">
-				<!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<?php foreach ($navLinks as $title => $link) : ?>
-						<li class="nav-item">
-							<a class="nav-link text-center" href="<?= $link ?>"><?= $title ?></a>
-						</li>
-					<?php endforeach; ?>
-					<!-<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-center" href="#" id="navbar-dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> My Interests </a>
-							<ul class="dropdown-menu p-0" aria-labelledby="navbar-dropdown">
-								<?php foreach ($interests as $title => $link) : ?>
-									<li class="dropdown-list-item">
-										<a class="dropdown-item text-center" href="<?= $link ?>"><?= $title ?></a>
-									</li>
-								<?php endforeach; ?>
-							</ul>
-						</li>
-					--
-				</ul> -->
-				<?php if (isset($_SESSION['loginSuccess']) && $_SESSION['loginSuccess'] === true) { ?>
-					<a class="nav-link text-center" href="login-profile.php"><?= $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] ?></a>
-					<a href="config/logout.php" class="btn btn-outline-danger" title="Log Out" id="logout-btn">
-						<i class="fa-solid fa-arrow-right-from-bracket"></i>
-					</a>
-				<?php
-				} else { ?>
-					<a href="login.php" class="btn btn-outline-success" title="Log In" id="login-btn">
-						<i class="fa-solid fa-arrow-right-to-bracket"></i>
-					</a>
-				<?php } ?>
-				<!-- log out button -->
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<?php foreach ($navLinks as $title => $link) : ?>
 						<li class="nav-item">
@@ -63,6 +33,18 @@ require_once 'config/nav-links.php';
 						</li>
 					-->
 				</ul>
+				<?php if (isset($_SESSION['loginSuccess']) && $_SESSION['loginSuccess'] === true) { ?>
+					<a class="nav-link text-center" href="login-profile.php"><?= $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] ?></a>
+					<a href="config/logout.php" class="btn btn-outline-danger" title="Log Out" id="logout-btn">
+						<i class="fa-solid fa-arrow-right-from-bracket"></i>
+					</a>
+				<?php
+				} else { ?>
+					<a href="login.php" class="btn btn-outline-success" title="Log In" id="login-btn">
+						<i class="fa-solid fa-arrow-right-to-bracket"></i>
+					</a>
+				<?php } ?>
+				<!-- log out button -->
 			</div>
 		</div>
 	</nav>
