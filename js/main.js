e3da829beb6dd3166d070b656d0ef47889af3295
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-15 11:43:02 -0400

    header mobile dropdown

--- js/main.js
+++ js/main.js
@@ -8,16 +8,3 @@ window.addEventListener('load', function (evt) {
         loader.classList.add('hidden');
     }
 });
-
-let toggleButton = document.getElementById('navbar-toggler');
-let toggleContent = document.getElementById('navbar-supported-content');
-/* 
-toggleButton.addEventListener('click', function (evt) {
-    if (!toggleContent.classList.contains('show')) {
-        toggleButton.setAttribute('aria-expanded', 'true');
-        toggleContent.classList.add('show');
-    } else if (toggleContent.classList.contains('show') || toggleButton.classList.contains('collapsed')) {
-        toggleButton.setAttribute('aria-expanded', 'false');
-        toggleContent.classList.remove('show');
-    }
-}); */
