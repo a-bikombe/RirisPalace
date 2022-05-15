4987e76fb9fe1f68da4ac6c4b68b1172fa941f37
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-14 22:06:02 -0400

    header banner

--- include/header.php
+++ include/header.php
@@ -37,12 +37,12 @@ require_once 'config/nav-links.php';
 					<!-- profile link -->
 					<a class="nav-link text-center" href="login-profile.php"><?= $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] ?></a>
 					<!-- log out button -->
-					<a href="config/logout.php" class="btn btn-outline-danger" title="Log Out" id="logout-btn">
+					<a href="config/logout.php" class="logout-btn btn btn-outline-danger" title="Log Out" id="logout-btn">
 						<i class="fa-solid fa-arrow-right-from-bracket"></i>
 					</a>
 				<?php } else { ?>
 					<!-- log in button -->
-					<a href="login.php" class="btn btn-outline-success" title="Log In" id="login-btn">
+					<a href="login.php" class="login-btn btn btn-outline-success" title="Log In" id="login-btn">
 						<i class="fa-solid fa-arrow-right-to-bracket"></i>
 					</a>
 				<?php } ?>
