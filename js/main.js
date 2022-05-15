56e2b4d67e4120f2c02d9828e3f91530cd16c2ab
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-15 11:18:11 -0400

    header mobile dropdown

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ toggleButton.addEventListener('click', function (evt) {
     if (!toggleContent.classList.contains('show')) {
         toggleButton.setAttribute('aria-expanded', 'true');
         toggleContent.classList.add('show');
-    } else if (toggleContent.classList.contains('show')) {
+    } else if (toggleContent.classList.contains('show') && toggleButton.classList.contains('collapsed')) {
         toggleButton.setAttribute('aria-expanded', 'false');
         toggleContent.classList.remove('show');
     }
