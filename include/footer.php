bf1689342c2c461ea24d6346217e30bc3c21b859
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-17 20:42:53 -0400

    practice

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,3 @@
 <footer>
-	<a href="#top" type="submit" class="footer-btn btn btn-light border-5px">Back To Top</a>
+	<a href="#top" type="submit" class="footer-btn btn btn-light border-1vw">Back To Top</a>
 </footer>
