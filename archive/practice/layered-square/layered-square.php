bb4f3137c87bdd7aa362f600ba30de341f52dbcf
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-17 20:15:09 -0400

    practice

--- archive/practice/layered-square/layered-square.php
+++ archive/practice/layered-square/layered-square.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> <?= strtolower($pageTitle) ?> </title>
-	<link src="layered-square.css" rel="stylesheet">
+	<link href="layered-square.css" rel="stylesheet">
 </head>
 
 <body>
