36dbe252ca3504c87ee32189cf565b14764e31a1
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-17 21:43:37 -0400

    sass

--- include/under-construction.php
+++ include/under-construction.php
@@ -1,4 +1,4 @@
 <div class="under-construction fixed flex-center vh-100 vw-100 dir-col">
-	<img src="images/gifs/corg-snail.gif" alt="Under Construction">
+	<img src="images/gifs/corg-books.gif" alt="Under Construction">
 	<p class="subtitle pt-1vw"><strong>This Page Is Under Construction. Check Back In Later!</strong></p>
 </div>
