18df114340c2d80cfda45ced88f157e87feaf5a3
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-17 21:45:12 -0400

    sass

--- include/loading-page.php
+++ include/loading-page.php
@@ -1,4 +1,4 @@
-<div class="loader fixed flex-center vh-100 vw-100 dir-col">
+<div class="loader overlay fixed flex-center vh-100 vw-100 dir-col">
 	<img src="images/gifs/corg-snail.gif" alt="Loading">
 	<p class="subtitle pt-1vw"><strong>Just A Moment!</strong></p>
 </div>
--- include/under-construction.php
+++ include/under-construction.php
@@ -1,4 +1,4 @@
-<div class="under-construction fixed flex-center vh-100 vw-100 dir-col">
+<div class="under-construction overlay fixed flex-center vh-100 vw-100 dir-col">
 	<img src="images/gifs/corg-books.gif" alt="Under Construction">
 	<p class="subtitle pt-1vw"><strong>This Page Is Under Construction. Check Back In Later!</strong></p>
 </div>
