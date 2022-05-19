b5e9cf94f826c8efdaa7bc7a0a9527e015e40942
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-18 21:13:57 -0400

    added jquery

--- js/kpop.js
+++ js/kpop.js
@@ -36,6 +36,16 @@ function toggleGroupInfo() {
 
 toggleGroupInfo();
 
+$(document).ready(function(){
+    if (jQuery) {  
+      // jQuery is loaded  
+      alert("Yeah!");
+    } else {
+      // jQuery is not loaded
+      alert("Doesn't Work");
+    }
+  });
+
 
 /* 
 groups.forEach(group => {
