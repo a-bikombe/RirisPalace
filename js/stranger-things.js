5b48d91a1b702146fadbd3f69dc09e7485d98450
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-18 21:49:11 -0400

    added jquery

--- js/stranger-things.js
+++ js/stranger-things.js
@@ -11,11 +11,11 @@ const s4Vol2 = new Date(2022, 06, 01);
 if (currentDate < s4Vol1) {
 	s4Vol1Countdown.show();
 	let days = (s4Vol1 - currentDate) / one_day;
-	daysUntilS4Vol1.textContent = parseInt(days);
+	daysUntilS4Vol1.text(parseInt(days));
 }
 
 if (currentDate >= s4Vol1 && currentDate < s4Vol2) {
 	s4Vol2Countdown.show();
 	let days = (s4Vol2 - currentDate) / one_day;
-	daysUntilS4Vol2.textContent = parseInt(days);
+	daysUntilS4Vol2.text(parseInt(days));
 }
