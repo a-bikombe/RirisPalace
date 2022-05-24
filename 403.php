a3017547776e66a7c0b6e79e6c99721895f4fea5
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-24 16:43:50 -0400

    episode thoughts

--- 403.php
+++ 403.php
@@ -19,7 +19,6 @@ $scripts = [
 <!DOCTYPE html>
 <html lang="en">
 
-<?php require_once 'include/loading-page.php'; ?>
 <?php require_once 'include/head-tag.php'; ?>
 
 <body class="dark-theme" id="error">
