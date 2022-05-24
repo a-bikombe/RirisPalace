38567e4543eef67b810fd4fc6884137bc5274ca9
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-24 16:03:06 -0400

    error pages

--- 403.php
+++ 403.php
@@ -1,17 +1,37 @@
+<?php 
+
+$pageTitle = 'I\'m an open book. Usually.';
+
+$iconPath = '/images/icons/403.png';
+
+$styles = [];
+
+$mobileStyles = [
+	'main' => '700'
+];
+
+$scripts = [
+	'main'
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
+<?php require_once 'include/head-tag.php'; ?>
+<!-- 
 <head><meta charset="utf-8">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I'm an open book. Usually.</title>
-    <link rel="icon" href="/images/icons/403.png">
+    <title></title>
+    <link rel="icon" href="">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="/css/main.css" rel="stylesheet">
     <link href="/css/main-mobile.css" rel="stylesheet" media="screen and (max-width:600px)">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
     <script src="https://kit.fontawesome.com/34e100283f.js" crossorigin="anonymous" defer></script>
-</head>
+</head> -->
 
 <body class="dark-theme" id="error">
     <main class="error-main flex-center text-center dir-col vh-100">
