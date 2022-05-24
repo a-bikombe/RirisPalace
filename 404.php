8448f0e5784ba3fbafb74b0be12e255c28eec634
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-24 16:52:17 -0400

    episode thoughts

--- 404.php
+++ 404.php
@@ -22,12 +22,12 @@ $scripts = [
 <?php require_once 'include/head-tag.php'; ?>
 
 <body class="dark-theme" id="error">
-    <?php require_once 'include/header.php'; ?>
     <main class="error-main flex-center text-center dir-col vh-100">
         <img src="/images/gifs/corg-sad.gif" alt="Corg Sad" class="gif">
         <section class="text" aria-label="404 Error Text">
             <h1>Uh Oh!</h1>
             <p>404.</p>
+            <p>Click <a href="/">here</a> to return to the homepage.</p>
         </section>
     </main>
 </body>
