3da2b88aa98306063a19b6186f062d1a8b2b3771
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-24 16:50:16 -0400

    episode thoughts

--- 403.php
+++ 403.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $pageTitle = 'I\'m an open book. Usually.';
 
@@ -7,11 +7,11 @@ $iconPath = 'images/icons/403.png';
 $styles = [];
 
 $mobileStyles = [
-	'main' => '600'
+    'main' => '600'
 ];
 
 $scripts = [
-	'main'
+    'main'
 ];
 
 ?>
@@ -22,6 +22,7 @@ $scripts = [
 <?php require_once 'include/head-tag.php'; ?>
 
 <body class="dark-theme" id="error">
+    <?php require_once 'include/header.php'; ?>
     <main class="error-main flex-center text-center dir-col vh-100">
         <img src="/images/gifs/corg-sad.gif" alt="Corg Sad" class="gif">
         <section class="text" aria-label="403 Error Text">
