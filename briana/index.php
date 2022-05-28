f1fc8dae85176245ec50bf0dee58b83594d3f941
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-28 14:46:51 -0400

    added briana dir

--- briana/index.php
+++ briana/index.php
@@ -1,13 +1,34 @@
 <!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>briana's page</title>
-	</head>
-	<body>
-		<h1>
-			Briana's world 
-		</h1>
-	</body>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>briana's page</title>
+</head>
+
+<body>
+
+	<header>
+		<h1>Briana's world</h1>
+	</header>
+
+	<main>
+		<h2>Chrisley Knows Best</h2>
+		<p>Chrisley Knows Best is my comfort show.</p>
+		<img src="https://pagesix.com/wp-content/uploads/sites/3/2022/05/todd-chrisley-78.jpg">
+	</main>
+
+	<footer></footer>
+
+</body>
+
 </html>
+
+<!-- 
+
+- Chrisley Knows Best
+- Comfort show
+- Pictures from the show
+
+ -->
