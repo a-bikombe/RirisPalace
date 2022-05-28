c8fa131ecc6ebb999f7d75bce2920a3bd91ca2e9
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-28 14:40:43 -0400

    added briana dir

--- briana/index.php
+++ briana/index.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>briana's page</title>
 	</head>
+	<body>
+		<h1>
+			Briana's world 
+		</h1>
+	</body>
 </html>
