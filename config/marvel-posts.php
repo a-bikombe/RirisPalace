e23288c4d86c220f77f8fc981c032d501c00d418
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-26 14:54:19 -0400

    marvel.php

---
+++ config/marvel-posts.php
@@ -0,0 +1,49 @@
+<?php
+
+$heroes = [
+	'spiderman' => [
+		'url' => '',
+		'image' => '',
+		'alt' => ''
+	]
+];
+
+$staticPosts = [
+	"monthly-hero" => [
+		"title" => "This Month's Hero",
+		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
+		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	],
+	"recent-release" => [
+		"title" => "Most Recent Release",
+		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
+		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	],
+	"peter-parker" => [
+		"title" => "In Defense of Peter Parker",
+		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
+		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	]
+];
+
+$dynamicPosts = [
+	"first-post" => [
+		"title" => "First Post",
+		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
+		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	],
+	"second-post" => [
+		"title" => "Second Post",
+		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
+		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	]
+];
+
+/* 
+New Post:
+"Most Recent Release" => [
+	"title" => "",
+	"image" => "",
+	"text" => ""
+]
+*/
--- marvel.php
+++ marvel.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'config/session.php';
+require_once 'config/marvel-posts.php';
 $pageTitle = 'Marvel';
+$headerTitle = 'Your Friendly Neighborhood Pitstop for Superheroes!';
 
 $iconPath = 'images/icons/spiderman.jpeg';
 
@@ -24,7 +26,50 @@ $scripts = [
     <?php require_once 'include/loading-page.php'; ?>
     <?php require_once 'include/header.php'; ?>
     <main>
-        <h1><?= $pageTitle ?></h1>
+        <h1><?= $headerTitle ?></h1>
+
+        <!-- Hero Scrollbar -->
+        <section class="heroes" aria-label="Heroes">
+            <?php foreach ($heroes as $heroId => $hero) : ?>
+                <div id="<?= $heroId ?>">
+                    <a href="<?= $hero['url'] ?>">
+                        <img src="<?= $hero['image'] ?>" alt="<?= $hero['alt'] ?>">
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </section>
+
+        <!-- Static Posts -->
+        <section class="posts" aria-label="Static Posts">
+            <!-- Monthly Hero Spotlight -->
+            <?php foreach ($staticPosts as $postId => $post) : ?>
+                <section class="post" aria-labelledby="<?= $postId ?>">
+                    <div class="post-image">
+                        <img src="<?= $post['image'] ?>" alt="<?= $popoststContent['title'] ?>">
+                    </div>
+                    <div class="post-text">
+                        <h2 id="<?= $postId ?>"><?= $post['title'] ?></h2>
+                        <p><?= $post['text'] ?></p>
+                    </div>
+                </section>
+            <?php endforeach; ?>
+        </section>
+
+        <section class="posts" aria-label="Dynamic Posts">
+            <!-- Monthly Hero Spotlight -->
+            <?php foreach ($dynamicPosts as $postId => $postContent) : ?>
+                <section class="post" aria-labelledby="<?= $postId ?>">
+                    <div class="post-image">
+                        <img src="<?= $postContent['image'] ?>" alt="<?= $postContent['title'] ?>">
+                    </div>
+                    <div class="post-text">
+                        <h2 id="<?= $postId ?>"><?= $postContent['title'] ?></h2>
+                        <p><?= $postContent['text'] ?></p>
+                    </div>
+                </section>
+            <?php endforeach; ?>
+        </section>
+
     </main>
     <?php require_once 'include/footer.php'; ?>
 </body>
