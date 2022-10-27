32040f51ef455327e2ca6d19ef9893f44856be49
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-27 15:34:51 -0400

    bold

--- config/marvel-posts.php
+++ config/marvel-posts.php
@@ -12,12 +12,12 @@ $staticPosts = [
 	"monthly-hero" => [
 		"title" => "This Month's Hero",
 		"image" => "https://www.cnet.com/a/img/resize/539c1411834da3afa8007fc034e0b8ae666df0e0/hub/2022/02/16/94fa45be-985b-4842-b47d-ddb93cd0b87d/ironheart-vol-1-1-bartel-variant-textless-1.jpg?auto=webp&width=1200",
-		"text" => "November's hero is the one and only Riri Williams, aka Ironheart! She's finally making her MCU debut in November 11th's Black Panther: Wakanda Forever. I'm so excited for her origin story, and to learn her connection to the Royal Family. I haven't finished her comic, but I'm looking forward to ."
+		"text" => "November's hero is the one and only <strong>Riri Williams, aka Ironheart</strong>! She's finally making her MCU debut in November 11th's Black Panther: Wakanda Forever. I'm so excited for her origin story, and to learn her connection to the Royal Family. I haven't finished her comic, but I'm looking forward to ."
 	],
 	"recent-release" => [
 		"title" => "Most Recent Release",
 		"image" => "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/danai-gurira-angela-bassett-black-panther-wakanda-forever-1664979991.jpg?crop=1.00xw:0.827xh;0,0.0377xh&resize=640:*",
-		"text" => "Black Panther: Wakanda Forever is 2022's final and most anticipated theatrical release! (This information is not confirmed by any official sources, but instead is solely my opinion.) MCU "
+		"text" => "<strong>Black Panther: Wakanda Forever</strong> is 2022's final and most anticipated theatrical release! (This information is not confirmed by any official sources, but instead is solely my opinion.) MCU "
 	],
 	"peter-parker" => [
 		"title" => "In Defense of Peter Parker",
