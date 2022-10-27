798aaf7c0b79c155e4e99939b363b5a4812d2f23
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-27 15:25:05 -0400

    images

--- config/marvel-posts.php
+++ config/marvel-posts.php
@@ -11,18 +11,18 @@ $heroes = [
 $staticPosts = [
 	"monthly-hero" => [
 		"title" => "This Month's Hero",
-		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
-		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		"image" => "https://www.cnet.com/a/img/resize/539c1411834da3afa8007fc034e0b8ae666df0e0/hub/2022/02/16/94fa45be-985b-4842-b47d-ddb93cd0b87d/ironheart-vol-1-1-bartel-variant-textless-1.jpg?auto=webp&width=1200",
+		"text" => "November's hero is the one and only Riri Williams, aka Ironheart! She's finally making her MCU debut in November 11th's Black Panther: Wakanda Forever. I'm so excited for her origin story, and to learn her connection to the Royal Family. I haven't finished her comic, but I'm looking forward to ."
 	],
 	"recent-release" => [
 		"title" => "Most Recent Release",
-		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
-		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		"image" => "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/danai-gurira-angela-bassett-black-panther-wakanda-forever-1664979991.jpg?crop=1.00xw:0.827xh;0,0.0377xh&resize=640:*",
+		"text" => "Black Panther: Wakanda Forever is 2022's final and most anticipated theatrical release! (This information is not confirmed by any official sources, but instead is solely my opinion.) MCU "
 	],
 	"peter-parker" => [
 		"title" => "In Defense of Peter Parker",
 		"image" => "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3",
-		"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		"text" => "Peter Parker, aka Spider-Man. A god among men. A ray of sunshine in a bleak world. I could say so much about him. I'm extremely excited for the release of Spider-Man: Across The Spider-Verse next June."
 	]
 ];
 
@@ -39,7 +39,7 @@ $dynamicPosts = [
 	]
 ];
 
-/* 
+/*
 New Post:
 "Most Recent Release" => [
 	"title" => "",
