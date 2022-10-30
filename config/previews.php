7b7b797cd7f2264ce0518f908f4f8e427aec9ced
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-29 20:33:10 -0400

    COTW description

--- config/previews.php
+++ config/previews.php
@@ -22,7 +22,7 @@ $celebrities = [
 	],
 	'Sam Kiszka' => [
 		'image' => 'https://i.pinimg.com/564x/f4/7c/10/f47c108b4e5e76536e30d558ee73fa44.jpg',
-		'description' => "Oh my goodness. Oh, sweet heavens. Have you SEEN him? AND he plays bass and piano, AND is still like a nice person. Personally, if I was that attractive I would make it everyone else",
+		'description' => "Oh my goodness. Oh, sweet heavens. Have you SEEN him? He plays bass and piano in the rock band Greta Van Fleet, and is only 23. If you can believe it, he's this good-looking and is still, like, a <em>nice person</em>. Personally, if I was that attractive I would make it everyone else's problem. But anyway, he has a very positive energy about him: very \"bright-eyed, bushy-tailed\", which I'm super into. Also, at the same time, he has the infamous \"tired eye\" look, which I'm also VERY into. Overall, 10/10.",
 		'link' => 'index.php'
 	]
 ];
